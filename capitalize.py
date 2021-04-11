558de300807f0ad1a4a09e8c8baa50e4b18425a4
Vivek Dadhich <ivivekdadhich@gmail.com>
2021-04-11 21:53:12 +0530

    Update capitalize.py

--- capitalize.py
+++ capitalize.py
@@ -1,6 +1,18 @@
+'''You are asked to ensure that the first and last names of people begin with a capital letter in their passports. 
+For example, alison heck should be capitalised correctly as Alison Heck.
+Given a full name, your task is to capitalize the name appropriately.'''
+
+
+
+
+
+
+
 def solve(s):
     
     for i in s.split():
+        # print(s.split())
+        # ['hello', 'world']
         s = s.replace(i,i.capitalize())
     return s 
     
